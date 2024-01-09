9669726bbc75b302cdbdd5e228b64424bea90ad2
rahul <rahultomar03187@gmail.com>
2024-01-09 12:00:13 +0530

    fix url

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -11,7 +11,7 @@ export const API_URL =
   "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=" +
   API_KEY;
 export const SUGGESSION_API_URL =
-  "http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=";
+  "https://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=";
 export const SEARCH_API_URL =
   "https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=50&key=" +
   API_KEY +
