287072186724821156c455801b0bd03b79d9b499
rahul <rahultomar03187@gmail.com>
2024-01-09 11:01:29 +0530

    refactor sidebar link

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,13 +1,8 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import { useLocation, useSearchParams } from "react-router-dom";
-import { IoMdHome } from "react-icons/io";
-import { SiYoutubeshorts } from "react-icons/si";
 import SideBarLink from "./SideBarLink";
-import { FaTrophy } from "react-icons/fa";
-import { IoMdMusicalNote } from "react-icons/io";
-import { SiYoutubegaming } from "react-icons/si";
-
+import { sidebarLinkData } from "../utils/constants";
 const SideBar = () => {
   const isSidebarOpen = useSelector((store) => store.app.isSidebarOpen);
   const [params] = useSearchParams();
@@ -15,45 +10,26 @@ const SideBar = () => {
   const v = params?.get("v");
   let classses = "shadow-lg h-full z-50";
   if (isSidebarOpen) {
-    classses +=
-      ` w-40 md:w-64 absolute bg-white ${location.pathname!=='/watch'?'md:relative':''}`
+    classses += ` w-40 md:w-64 absolute bg-white ${
+      location.pathname !== "/watch" ? "md:relative" : ""
+    }`;
   } else {
-    classses += ` w-20 md:w-24 ${location.pathname==='/watch'?'hidden':''}`;
+    classses += ` w-20 md:w-24 ${
+      location.pathname === "/watch" ? "hidden" : ""
+    }`;
   }
 
   return (
     <div className={classses}>
       <ul className="py-4 px-3 md:px-7">
-        <SideBarLink
-          isActive={v === null}
-          Icon={IoMdHome}
-          label="Home"
-          to={"/"}
-        />
-        <SideBarLink
-          isActive={v === "1"}
-          Icon={SiYoutubeshorts}
-          label="Shorts"
-          to={"/?v=1"}
-        />
-        <SideBarLink
-          isActive={v === "17"}
-          Icon={FaTrophy}
-          label="Sports"
-          to={"/?v=17"}
-        />
-        <SideBarLink
-          isActive={v === "10"}
-          Icon={IoMdMusicalNote}
-          label="Music"
-          to={"/?v=10"}
-        />
-        <SideBarLink
-          isActive={v === "20"}
-          Icon={SiYoutubegaming}
-          label="Gaming"
-          to={"/?v=20"}
-        />
+        {sidebarLinkData.map((item) => (
+          <SideBarLink
+            isActive={item.v === v}
+            Icon={item.Icon}
+            label={item.label}
+            to={item.to}
+          />
+        ))}
       </ul>
     </div>
   );
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,25 +1,80 @@
+import { IoMdHome } from "react-icons/io";
+import { SiYoutubeshorts } from "react-icons/si";
+import { FaTrophy } from "react-icons/fa";
+import { IoMdMusicalNote } from "react-icons/io";
+import { SiYoutubegaming } from "react-icons/si";
+import { IoNewspaper } from "react-icons/io5";
+
 const API_KEY = process.env.REACT_APP_API_KEY;
 export const CHAT_LENGTH = 20;
-export const API_URL = 'https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key='+API_KEY;
-export const SUGGESSION_API_URL ='http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q='
-export const SEARCH_API_URL = 'https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=50&key='+API_KEY+'&q=';
-export const COMMENTS_API_URL = 'https://youtube.googleapis.com/youtube/v3/commentThreads?part=snippet%2Creplies&key='+API_KEY+"&maxResults=100&videoId=";
+export const API_URL =
+  "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=" +
+  API_KEY;
+export const SUGGESSION_API_URL =
+  "http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=";
+export const SEARCH_API_URL =
+  "https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=50&key=" +
+  API_KEY +
+  "&q=";
+export const COMMENTS_API_URL =
+  "https://youtube.googleapis.com/youtube/v3/commentThreads?part=snippet%2Creplies&key=" +
+  API_KEY +
+  "&maxResults=100&videoId=";
 
 export const CATEGORIES = [
-    "All",
-    "JavaScript",
-    "TypeScript",
-    "Programming",
-    "Travelling",
-    "Cricket World Cup 2023",
-    "Bowling",
-    "Cricket",
-    "React",
-    "Next.js",
-    "UI/UX Design",
-    "Object Oriented Programming",
-    "Frontend Web Development",
-    "Backend Web Development",
-    "Web Development",
-    "Coding",
-]
+  "All",
+  "JavaScript",
+  "TypeScript",
+  "Programming",
+  "Travelling",
+  "Cricket World Cup 2023",
+  "Bowling",
+  "Cricket",
+  "React",
+  "Next.js",
+  "UI/UX Design",
+  "Object Oriented Programming",
+  "Frontend Web Development",
+  "Backend Web Development",
+  "Web Development",
+  "Coding",
+];
+
+export const sidebarLinkData = [
+  {
+    Icon: IoMdHome,
+    label: "Home",
+    to: "/",
+    v: null
+  },
+  {
+    Icon: SiYoutubeshorts,
+    label: "Shorts",
+    to: "/?v=1",
+    v: '1'
+  },
+  {
+    Icon: FaTrophy,
+    label: "Sports",
+    to: "/?v=17",
+    v: '17'
+  },
+  {
+    Icon: IoMdMusicalNote,
+    label: "Music",
+    to: "/?v=10",
+    v: '10'
+  },
+  {
+    Icon: SiYoutubegaming,
+    label: "Gaming",
+    to: "/?v=20",
+    v: '20'
+  },
+  {
+    Icon: IoNewspaper,
+    label: "News",
+    to: "/?v=25",
+    v: '25'
+  },
+];
