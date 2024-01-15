dc99fdf870b4cfe0e1371d58e55048c422656100
rahul <rahultomar03187@gmail.com>
2024-01-15 11:13:36 +0530

    fix sidebar in small screen

--- src/components/VideoContainer.js
+++ src/components/VideoContainer.js
@@ -3,17 +3,40 @@ import useGetData from "../hooks/useGetData";
 import { API_URL } from "../utils/constants";
 import VideoCard from "./VideoCard";
 import Shimmer from "./Shimmer";
+import { useLayoutEffect } from "react";
+import { useDispatch } from "react-redux";
+import { closeSidebar } from "../store/appSlice";
 const VideoContainer = () => {
   const [searchParams] = useSearchParams();
+  const dispatch = useDispatch();
   const v = searchParams?.get("v");
   let url = API_URL;
   if (v !== null) {
     url = url + "&videoCategoryId=" + v;
   }
   const { data: videos, error } = useGetData(url, v);
+
+  useLayoutEffect(() => {
+    handleResize();
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+  
+  function handleResize() {
+    if (window.innerWidth < 1200) {
+      dispatch(closeSidebar());
+    }
+  }
+
   if (videos?.length === 0 && !error) return <Shimmer />;
-  else if(error){
-    return <p className="mx-auto my-8 w-[80vw] text-center text-3xl font-bold">Could not fetch Videos.</p>
+  else if (error) {
+    return (
+      <p className="mx-auto my-8 w-[80vw] text-center text-3xl font-bold">
+        Could not fetch Videos.
+      </p>
+    );
   }
   return (
     <>
