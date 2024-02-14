28ed9595b053e06cbc26950f50e7e8f71300cc1e
rahul <rahultomar03187@gmail.com>
2024-02-14 16:41:54 +0530

    fix videocard

--- src/components/VideoCard.js
+++ src/components/VideoCard.js
@@ -1,21 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import { abbreviateNumber } from "../utils/helper";
 import { formatDuration } from "../utils/formatDuration";
 import { formatTimeAgo } from "../utils/formatTimeAgo";
 
 const VideoCard = ({ info }) => {
+  const [isHovered, setIsHovered] = useState(false);
   const { snippet, statistics, contentDetails } = info;
   const { thumbnails, channelTitle, title, publishedAt } = snippet;
   const views = abbreviateNumber(statistics?.viewCount);
   return (
-    <div className="relative flex flex-col gap-2 p-3 m-2 group">
-      <img
-        className="rounded-xl group-hover:rounded-none group-hover:scale-105 duration-200"
-        alt="thumbnail"
-        src={thumbnails.medium.url}
-      />
-      <div className="absolute bottom-36 mx-5 right-1 bg-secondary-dark text-secondary text-sm px-0.5 rounded">
-        {formatDuration(contentDetails.duration)}
+    <div
+      className="flex flex-col gap-2 p-3 m-2"
+      onMouseOver={() => setIsHovered(true)}
+      onMouseOut={() => setIsHovered(false)}>
+      <div className="relative">
+        {isHovered && (
+          <div className="absolute inset-0 bg-black bg-opacity-50 flex items-center cursor-pointer justify-center rounded-2xl">
+            <iframe
+              width="100%"
+              height="100%"
+              src={`https://www.youtube.com/embed/${info?.id}?autoplay=1&mute=1`}
+              title={title}
+              frameBorder="0"
+              allowFullScreen
+              autoPlay
+              className="rounded-xl"></iframe>
+          </div>
+        )}
+        <img
+          className="rounded-xl relative -z-10 w-full"
+          alt="thumbnail"
+          src={thumbnails.medium.url}
+        />
+        <div className="absolute bottom-5 mx-5 right-1 bg-secondary-dark text-secondary text-sm px-0.5 rounded">
+          {formatDuration(contentDetails.duration)}
+        </div>
       </div>
       <div className="flex gap-1">
         <img
