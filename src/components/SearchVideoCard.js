9580afc17f9e0e1a89841f02cc68ca4d603528de
STRahul <rahuiltomar03187@gmail.com>
2024-09-08 23:46:37 +0530

    fix: viewcount in search video

--- src/components/SearchVideoCard.js
+++ src/components/SearchVideoCard.js
@@ -1,16 +1,16 @@
 import useGetData from "../hooks/useGetData";
-import { CHANNEL_INFO_API } from "../utils/constants";
+import { CHANNEL_INFO_API, VIDEO_DETAILS_API } from "../utils/constants";
 import { formatTimeAgo } from "../utils/formatTimeAgo";
 import { abbreviateNumber } from "../utils/helper";
 
 const SearchVideoCard = ({ info }) => {
   const { snippet } = info;
-  const { thumbnails, channelTitle, title, publishedAt } = snippet;
-  const {data: channelData} = useGetData(CHANNEL_INFO_API+ "&id="+ snippet?.channelId, snippet?.channelId);
-  const channelPicture = channelData?.[0]?.snippet?.thumbnails?.default?.url || 
-  'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT5958mvxyOALrWelcizzxdX48KqChi9Vh2Sr_NETQ&s';
-  const viewCount = abbreviateNumber(channelData?.[0]?.statistics?.viewCount);
-
+  const { thumbnails, channelTitle, title, publishedAt, channelId } = snippet;
+  const { data: channelData } = useGetData(CHANNEL_INFO_API + "&id=" + channelId, channelId);
+  const { data: videoDetails } = useGetData(VIDEO_DETAILS_API + "&id=" + info?.id?.videoId);
+  const channelPicture = channelData?.[0]?.snippet?.thumbnails?.default?.url ||
+    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT5958mvxyOALrWelcizzxdX48KqChi9Vh2Sr_NETQ&s';
+  const viewCount = abbreviateNumber(videoDetails?.[0]?.statistics?.viewCount);
   return (
     <div className="flex md:p-3 md:gap-3 mx-4 flex-col md:flex-row">
       <div className="md:p-3 m-2">
@@ -27,7 +27,7 @@ const SearchVideoCard = ({ info }) => {
         <div className="flex">
           <span className="pl-2">{viewCount} views</span>
           <p className="before:content-['•'] before:mx-1">
-              {formatTimeAgo(publishedAt)}
+            {formatTimeAgo(publishedAt)}
           </p>
         </div>
         <div className="flex items-center py-2">
